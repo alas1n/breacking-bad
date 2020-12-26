e6f186d956ad85215f9804ca1978567471bf5edd
ali <amirzaei26@gmail.com>
2020-12-26 04:21:54 +0330

    Interrapt code fixed

---
+++ arduino/interrupt/interrupt.ino
@@ -0,0 +1,24 @@
+void ICACHE_RAM_ATTR handleInterrupt ();
+
+const int interruptPin = 5; //GPIO 0 = (Flash Button) 
+const int LED=2; //On board blue LED 
+
+void setup() { 
+  Serial.begin(115200); 
+  pinMode(LED,OUTPUT); 
+  pinMode(interruptPin, INPUT_PULLUP); 
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, RISING); 
+} 
+
+void loop() 
+{ 
+    digitalWrite(LED,HIGH); //LED off 
+    delay(1000); 
+    digitalWrite(LED,LOW); //LED on 
+    delay(1000); 
+} 
+
+//This program get executed when interrupt is occures i.e.change of input state
+void handleInterrupt() { 
+    Serial.println("Interrupt Detected"); 
+}
